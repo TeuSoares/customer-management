c7f6f0b925d0ea98ee2a4e7401e91b27a0747323
Mateus Soares <mateussoares114@gmail.com>
2024-03-19 20:34:43 -0300

    style: changing label

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="p-4 bg-[#111111] text-center">
       <span className="text-white">
-        Copyright © 2024 - Development by Mateus Soares
+        Copyright © 2024 - Desenvolvido por Mateus Soares
       </span>
     </footer>
   )
